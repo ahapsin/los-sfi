2b81e014cdf2b40a67c81b95128f87fecf7db6e5
ahapsin <ahapsin02@gmail.com>
2024-10-24 08:08:09 +0700

    rollup

--- src/views/pages/TestView.vue
+++ src/views/pages/TestView.vue
@@ -1,22 +1,3 @@
 <template>
-  <n-data-table :columns="columns" :data="list" :loading="loading" />
-  <n-button @click="survey.fetchData">mount</n-button>
-  <n-button @click="survey.$reset">reset</n-button>
+  <div>sadsad</div>
 </template>
-<script setup>
-import { storeToRefs } from "pinia";
-import { useSurveyStore } from "../../stores/survey";
-const survey = useSurveyStore();
-const { list, loading } = storeToRefs(survey);
-const columns = [
-  {
-    title: "No",
-    key: "id",
-    sorter: "default",
-  },
-  {
-    title: "Title",
-    key: "nama_debitur",
-  },
-];
-</script>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+// vite.config.ts
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,12 +11,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
+          // Example: Splitting specific libraries
+          if (id.includes('vue3-pdfmake')) {
+            return 'vuepdf';
           }
         },
       },
