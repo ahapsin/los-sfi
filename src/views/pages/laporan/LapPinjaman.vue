436e1e7bd47edd543a281645c33944f1e4e355aa
ahapsin <ahapsin02@gmail.com>
2024-12-24 09:42:28 +0700

    arus kas

--- src/views/pages/laporan/LapPinjaman.vue
+++ src/views/pages/laporan/LapPinjaman.vue
@@ -2,71 +2,30 @@
   <div>
     <n-space vertical>
       <n-card :title="`Laporan Keuangan`">
-        <n-tabs type="card" animated @before-leave="handleBeforeLeave">
-<!--          <n-tab-pane name="pinjaman" tab="PINJAMAN"> Informasi Pinjaman</n-tab-pane>-->
-<!--          <n-tab-pane name="pembayaran" tab="PEMBAYARAN">-->
-<!--            Informasi Pembayaran-->
-<!--          </n-tab-pane>-->
-<!--          <n-tab-pane name="jaminan" tab="JAMINAN"> Informasi Jaminan</n-tab-pane>-->
-<!--          <n-tab-pane name="tunggakan" tab="TUNGGAKAN"> Informasi Tunggakan</n-tab-pane>-->
-          {{handleRange}}
-          <n-tab-pane name="arus kas" tab="ARUS KAS">
-            <TabArusKas :data="dataArusKas" :columns="columns" :load="loadData" :page-size="10" :range="handleRange"/>
+        <n-tabs type="card" animated @before-leave="handleBeforeLeave" @update:value="handleUpdateValue">
+          <!--          <n-tab-pane name="pinjaman" tab="PINJAMAN"> Informasi Pinjaman</n-tab-pane>-->
+          <!--          <n-tab-pane name="pembayaran" tab="PEMBAYARAN">-->
+          <!--            Informasi Pembayaran-->
+          <!--          </n-tab-pane>-->
+          <!--          <n-tab-pane name="jaminan" tab="JAMINAN"> Informasi Jaminan</n-tab-pane>-->
+          <!--          <n-tab-pane name="tunggakan" tab="TUNGGAKAN"> Informasi Tunggakan</n-tab-pane>-->
+
+          <n-tab-pane name="inq_pinjaman" tab="INQUERY PINJAMAN">
+           <TabInqPinjaman :columns="columnsPinjaman" :data="dataInqPinjaman"/>
           </n-tab-pane>
+          <n-tab-pane name="arus_kas" tab="ARUS KAS">
+            <TabArusKas :data="dataArusKas" :columns="columns" :load="loadData" :page-size="10"
+                        @change-date-range="handleRange"/>
+          </n-tab-pane>
+
         </n-tabs>
-        <template #header-extra>
-          <n-space class="!gap-1">
-            <div class="me-1 flex gap-2">
-              <n-date-picker
-                  v-model:value="timestamp"
-                  type="month"
-                  :default-value="Date.now()"
-                  format="y MMM"
-                  month-format="MMM"
-                  clearable
-              />
-              <n-popover trigger="click" placement="bottom-end">
-                <template #trigger>
-                  <n-button circle>
-                    <n-icon>
-                      <search-icon/>
-                    </n-icon>
-                  </n-button>
-                </template>
-                <n-input
-                    autofocus="true"
-                    clearable
-                    placeholder="cari disini.."
-                    v-model:value="searchBox"
-                />
-              </n-popover>
-            </div>
-            <div>
-              <n-button circle type="success" secondary>
-                <template #icon>
-                  <n-icon>
-                    <download-icon/>
-                  </n-icon>
-                </template>
-              </n-button>
-            </div>
-            <div class="md:hidden">
-              <n-button type="primary" @click="handleAdd">
-                <template #icon>
-                  <n-icon>
-                    <add-icon/>
-                  </n-icon>
-                </template>
-              </n-button>
-            </div>
-          </n-space>
-        </template>
+
       </n-card>
     </n-space>
   </div>
 </template>
 <script setup>
-import {ref, onMounted, h, computed} from "vue";
+import {ref, h, computed} from "vue";
 import {useApi} from "../../../helpers/axios";
 import {useSearch} from "../../../helpers/searchObject";
 import router from "../../../router";
@@ -74,20 +33,15 @@ import {
   useDialog,
   useMessage,
   NIcon,
-  NButton,
 } from "naive-ui";
-import {
-  AddCircleOutlineRound as AddIcon,
-  SearchOutlined as SearchIcon,
-  FileDownloadOutlined as DownloadIcon,
-} from "@vicons/material";
 import {
   DeleteOutlined as DeleteIcon,
   ListAltOutlined as DetailIcon,
 } from "@vicons/material";
 import TabArusKas from "./TabArusKas.vue";
+import TabInqPinjaman from "./TabInqPinjaman.vue";
 
-const handleRange = ref();
+const rangeDate = ref();
 const message = useMessage();
 const dialog = useDialog();
 const dataTable = ref([]);
@@ -134,31 +88,74 @@ const columns = [
     key: "ORIGINAL_AMOUNT",
     sorter: "default",
     render(row) {
-      return h("div", row.ORIGINAL_AMOUNT.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
+      return h("div", row.ORIGINAL_AMOUNT.toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+      }));
     }
   },
 ];
+const columnsPinjaman = [
+  {
+    title: "Nomor Order",
+    key: "ORDER_NUMBER",
+    sorter: "default",
+  }, {
+    title: "Nama Debitur",
+    key: "customer_name",
+    sorter: "default",
+  }, {
+    title: "Nomor Polisi",
+    key: "POLICE_NUMBER",
+    sorter: "default",
+  }, {
+    title: "Tanggal Pencairan",
+    key: "ENTRY_DATE",
+    sorter: "default",
+  },{
+    title: "Cabang",
+    key: "branch_name",
+    sorter: "default",
+  },
+  {
+    title: "Detail",
+    key: "ENTRY_DATE",
+    sorter: "default",
+  },
+
+];
 const loadData = ref(false);
+const handleRange = (value) => {
+  rangeDate.value = value;
+  getArusKas(value);
+}
 
 const handleBeforeLeave = async (t) => {
   switch (t) {
-    case "arus kas":
+    case "arus_kas":
       await getArusKas();
+      return true;
+    case "inq_pinjaman":
+      await getInqPinjaman();
       return true;
     default:
       return true;
   }
 }
 
-const getArusKas = async () => {
+const handleUpdateValue = (value) => {
+
+}
+
+const getArusKas = async (e) => {
   message.loading('memuat data arus kas');
   let userToken = localStorage.getItem("token");
   const response = await useApi({
     method: "POST",
     api: "arus_kas",
-    data:{
-      dari : "2024-12-16",
-      sampai : "2024-12-16"
+    data: {
+      dari: e ? e[0] : "",
+      sampai: e ? e[1] : "",
     },
     token: userToken,
   });
@@ -169,6 +166,24 @@ const getArusKas = async () => {
     dataArusKas.value = response.data;
   }
 }
+
+const dataInqPinjaman = ref();
+const getInqPinjaman = async (e) => {
+  message.loading('memuat data arus kas');
+  let userToken = localStorage.getItem("token");
+  const response = await useApi({
+    method: "GET",
+    api: "creditReport",
+    token: userToken,
+  });
+  if (!response.ok) {
+    message.error('ERROR API');
+  } else {
+    loadData.value = false;
+    dataInqPinjaman.value = response.data;
+  }
+}
+
 const handleConfirm = (row, index) => {
   dialog.warning({
     title: "Confirm",
@@ -194,15 +209,19 @@ const handleConfirm = (row, index) => {
     },
   });
 };
+
 const handleDetail = (evt) => {
   router.push(`/master/branch-action/${evt.id}/detail`);
 };
+
 const handleUpdate = (evt) => {
   router.push(`/master/branch-action/${evt.id}`);
 };
+
 const handleAdd = () => {
   router.push("/master/branch-action");
 };
+
 const getData = async () => {
   let userToken = localStorage.getItem("token");
   const response = await useApi({
@@ -218,6 +237,7 @@ const getData = async () => {
     dataTable.value = response.data;
   }
 };
+
 const renderIcon = (icon) => {
   return () => {
     return h(NIcon, null, {
@@ -225,6 +245,7 @@ const renderIcon = (icon) => {
     });
   };
 };
+
 const options = [
   {
     label: "Hapus",
@@ -237,10 +258,11 @@ const options = [
     icon: renderIcon(DetailIcon),
   },
 ];
+
 const pagination = {
   pageSize: 10,
 };
-onMounted(() => getArusKas());
+
 const showData = computed(() => {
   return useSearch(dataTable.value, searchBox.value);
 });
---
+++ src/views/pages/laporan/TabInqPinjaman.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <n-space vertical :size="12" class="pt-4">
+        <n-space>
+          <n-input placeholder="cari"/>
+          <n-button @click="downloadCsv">download</n-button>
+        </n-space>
+        <n-data-table :columns="columns" :data="props.data"/>
+      </n-space>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  columns: Array,
+  data:Object
+})
+</script>
