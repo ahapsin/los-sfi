50f63bb2e3710119172bfb9bb716c76d3d85c676
ahapsin <ahapsin02@gmail.com>
2025-02-21 16:28:09 +0700

    app logo

--- src/components/layout/SingleSideBar.vue
+++ src/components/layout/SingleSideBar.vue
@@ -2,77 +2,85 @@
   <n-layout class="h-screen">
     <n-layout-header style="height: 64px" bordered>
       <n-page-header
-        class="sticky bg-sc-50 border-b top-0 z-50 backdrop-blur p-4"
+          class="sticky bg-sc-50 border-b top-0 z-50 backdrop-blur p-4"
       >
         <template #title>
           <n-space align="center">
             <n-button
-              circle
-              quaternary
-              @click="sideMenu.sideEffect = !sideMenu.sideEffect"
-              color="#424242"
+                circle
+                quaternary
+                @click="sideMenu.sideEffect = !sideMenu.sideEffect"
+                color="#424242"
             >
               <template #icon>
-                <n-icon v-if="sideMenu.sideEffect"><burger-icon /></n-icon>
-                <n-icon v-else><close-icon /></n-icon>
+                <n-icon v-if="sideMenu.sideEffect">
+                  <burger-icon/>
+                </n-icon>
+                <n-icon v-else>
+                  <close-icon/>
+                </n-icon>
               </template>
             </n-button>
             <img
-              class="h-10 md:h-10"
-              :src="applogo"
-              alt="logo_company"
+                class="h-10 md:h-10"
+                :src="applogo"
+                alt="logo_company"
             />
             <div class="flex flex-col items-left justify-center">
-              <span>{{apptitle}}</span>
-            <span class="text-[10px]">v. {{ appVersion}}</span>
+              <span>{{ apptitle }}</span>
+              <span class="text-[10px]">v. {{ appVersion }}</span>
             </div>
           </n-space>
           <span class="hidden md:flex capitalize"></span>
         </template>
         <template #extra>
           <div class="flex items-center">
-            <account-avatar />
+            <account-avatar/>
           </div>
         </template>
       </n-page-header>
     </n-layout-header>
     <n-layout position="absolute" style="top: 70px" has-sider>
       <n-layout-sider
-        :width="180"
-        :collapsed-width="0"
-        :show-collapsed-content="false"
-        :collapsed="sideMenu.sideEffect ? true : false"
-        content-style="padding: 10px;"
-        class="bg-white/80 backdrop-blur absolute md:relative h-full z-20"
-        bordered
+          :width="180"
+          :collapsed-width="0"
+          :show-collapsed-content="false"
+          :collapsed="sideMenu.sideEffect ? true : false"
+          content-style="padding: 10px;"
+          class="bg-white/80 backdrop-blur absolute md:relative h-full z-20"
+          bordered
       >
-        <SideMenu />
+        <SideMenu/>
       </n-layout-sider>
-      <n-layout class="bg-white p-2 md:p-4" :native-scrollbar="false">
-        <RouterView />
+      <n-layout :native-scrollbar="false"
+                :class="`p-2 md:p-4 bg-gradient-to-t from-slate-100`">
+        <RouterView/>
         <slot/>
       </n-layout>
     </n-layout>
   </n-layout>
   <n-drawer v-model:show="active" placement="left">
     <n-drawer-content>
-      <logo-header />
-      <side-menu />
+      <logo-header/>
+      <side-menu/>
     </n-drawer-content>
   </n-drawer>
 </template>
 <script setup>
-import { ref, onMounted } from "vue";
+import {ref, onMounted} from "vue";
 import {
   LayoutSidebarLeftCollapse as CloseIcon,
   LayoutSidebarRightCollapse as BurgerIcon,
 } from "@vicons/tabler";
-import { useSidebar } from "../../stores/sidebar";
-import { useWindowSize } from "@vueuse/core";
+import {useSidebar} from "../../stores/sidebar";
+import {useWindowSize} from "@vueuse/core";
 import pjson from '../../../package.json';
+
 const applogo = import.meta.env.VITE_APP_LOGO;
+const appbase = import.meta.env.VITE_APP_BASE_COLOR;
+const appbackdrop = import.meta.env.VITE_APP_BACKDROP;
 const appVersion = pjson.version;
-const { width } = useWindowSize();
+const {width} = useWindowSize();
 
 const sideMenu = useSidebar();
 const active = ref(false);
--- src/views/pages/DashboardPage.vue
+++ src/views/pages/DashboardPage.vue
@@ -1,1 +1,31 @@
-<template></template>
+<template>
+  <n-card>Hi,<b>{{me.me.nama}} 👋</b>
+  </n-card>
+</template>
+<script setup>
+import _ from "lodash";
+import {useMessage} from "naive-ui";
+import {useApi} from "../../helpers/axios.js";
+import {useMeStore} from "../../stores/me.js";
+const appbackdrop = import.meta.env.VITE_APP_BACKDROP;
+const me=useMeStore();
+const message = useMessage();
+const dataMenu = ref();
+const getMenu = async () => {
+  let userToken = localStorage.getItem("token");
+  const response = await useApi({
+    method: 'GET',
+    api: 'menu-sub-list',
+    token: userToken
+  });
+  if (!response.ok) {
+    message.error(response.error.data.message);
+  } else {
+    dataMenu.value = response.data.response;
+  }
+}
+onMounted(() => {
+  getMenu();
+})
+
+</script>
